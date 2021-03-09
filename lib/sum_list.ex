ec6be1e1e7d96ef43e0bee7c5fab61c6758a3390
Ayrton Souza <ayrtonsouza@icloud.com>
2021-03-08 21:56:29 -0300

    development with enum module

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,6 +1,8 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
+  def call_enum(list), do: Enum.any?(list, fn element -> element > 5 end)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
